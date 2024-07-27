67828a32d2cbca7df5b5b2904c4f2df296f457e0
Daniel Kassaye <danielkassaye@daniels-air-4.mynetworksettings.com>
2024-07-27 13:57:06 -0700

    first commit

---
+++ hw5.py
@@ -0,0 +1,26 @@
+#Assignments: Python File Handling
+'''
+1. Exploring Python's OS Module
+
+Objective: The goal of this assignment is to deepen your understanding of the OS module in Python.
+
+Task 1: Directory Inspector:
+
+Problem Statement: Create a Python script that lists all files and subdirectories in a given directory.
+Your script should prompt the user for the directory path and then display the contents.
+
+Code Example:
+    import os
+
+    def list_directory_contents(path):
+        # List and print all files and subdirectories in the given path
+Expected Outcome: The script should correctly list all files and subdirectories in the specified directory. 
+Handle exceptions for invalid paths or inaccessible directories.
+NOTE: Ensure that all code in your file is ready to run. This means that if someone opens your file and clicks 
+the run button at the top, all code executes as intended. For example, if there are if statements, print statements, 
+or for loops, they should function correctly and display output in the console as expected. If you have a function, 
+make sure the function is called and runs.
+
+The goal of this note is to ensure that all code in your Python file runs smoothly and that is has been tested.
+
+'''
---
+++ notes.py
@@ -0,0 +1,27 @@
+'''
+This is a note for Module 3 of Python Intermediate. 
+This is a Multi-line String comment. 
+
+Lesson :4 Python File Handling
+
+# There are four collection data types in the Python programming language: List, Tuple, Set, and Dictionary.
+
+https://www.w3schools.com/python/python_file_handling.asp
+
+https://www.tutorialspoint.com/python/file_seek.htm
+
+
+
+'''
+
+# File handling is an important part of any web application.
+
+# Python has several functions for creating, reading, updating, and deleting files.
+
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demofile3.txt", "r")
+print(f.read())
